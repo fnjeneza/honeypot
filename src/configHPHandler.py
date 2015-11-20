d04385efb093a60c6ff4c667d7e9f66a80444581
Njeneza François <21510421@etu.unicaen.fr>
2015-11-20 12:43:29 +0100

    simplify the conf file and configuration handler

--- src/configHPHandler.py
+++ src/configHPHandler.py
@@ -2,86 +2,41 @@
 #-*-coding:utf-8*-*
 __author__ = 'fnjeneza'
 
-import logging
-from logging.handlers import RotatingFileHandler
 from os import path
 
 """
-Gestion de  la configuration
+Configuration handler
 """
 
-
-#chemin du dossier de logs par défaut, 
-#peut être modifié dans la config
-log_dir=""
-
-#log
-logger = logging.getLogger('HoneyPot')
-
-def log_config(logger, log_dir):
+def load_configuration(config_file=None):
     """
-    charge le niveau, le formattage, les handlers
+    Load configuration
+
+    @param config_file: Path to config file
+    Raise exceptions if no config file is found
+    or syntax is incorrect
+    @return configs: configurations map {key:value}
     """
-    logger = logging.getLogger('HoneyPot')
-    logger.setLevel(logging.INFO)
+    if config_file is None:
+        config_file ="config/main.conf"
+        
+    config={} # configuration parameters
+    if path.exists(config_file):
+        with open(config_file) as conf:
+            nb_line=0
+            for line in conf.readlines():
+                nb_line+=1
+                line = line.strip()
+                if line=='' or line[0]=="#":
+                    continue
+                
+                line = line.split()
+                if len(line)!=2:
+                    raise Exception("Incorrect syntax, line "+str(nb_line))
 
-    formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
+                config[line[0].strip()] = line[1].strip()
 
-#    critic_handler = RotatingFileHandler(log_dir+'critic.log', 'a', 1000000, 1)
-#    warn_handler = RotatingFileHandler(log_dir+'warn.log', 'a', 1000000, 1)
-    info_handler = RotatingFileHandler(log_dir+'info.log', 'a', 1000000, 1)
-
-#    critic_handler.setLevel(logging.CRITICAL)
-#    warn_handler.setLevel(logging.WARNING)
-    info_handler.setLevel(logging.DEBUG)
-
-#    critic_handler.setFormatter(formatter)
-#    warn_handler.setFormatter(formatter)
-    info_handler.setFormatter(formatter)
-
-#    logger.addHandler(critic_handler)
-#    logger.addHandler(warn_handler)
-    logger.addHandler(info_handler)
-
-
-######## charge les logs par défaut ####
-log_config(logger, log_dir)
-
-####################################
-## Chargement de la configuration ##
-####################################
-config_file ="config/main.cfg" #config file
-config={} # les paramètres de configuration
-if path.exists(config_file):
-    with open(config_file) as conf:
-        nb_line=0
-        for line in conf.readlines():
-            nb_line+=1
-            line = line.strip()
-            if line=='' or line[0]=="#":
-                continue
-            
-            line = line.split('=')
-            if len(line)!=2:
-                logger.critical("Configuration incorrecte ligne "+str(nb_line))
-                logger.critical("Arrêt du programme")
-                exit()
-
-            config[line[0].strip()] = line[1].strip()
-
-else:
-    logger.critical('Fichier de configuration non trouvé')
-    logger.critical('Arrêt du programme')
-    exit()
+    else:
+        raise Exception('Configuration file unfound')
     
-## recharge le dossier des logs en fonction de la config
-try:
-    log_dir = config['log_dir']
-    if log_dir!='':
-        log_config(logger, log_dir)
-except:
-    pass
-
-#logger.debug("hello world")
-#logger.warn("this is a warning")
-#logger.critical("ohhh! This is critical")
+    return configs
