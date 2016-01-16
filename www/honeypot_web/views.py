61b508ff5cb15edecab0aa173f19bd083f103b98
François Njeneza <njenezamed@gmail.com>
2016-01-16 20:18:44 +0100

    index view

--- www/honeypot_web/views.py
+++ www/honeypot_web/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html',{'last_urls':['tet','de']})
+
+def received(request):
+    return render(request,'received.html')
