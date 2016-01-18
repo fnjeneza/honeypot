fa28b6d470a987ddf52f19d2000412e16fa7cca6
François Njeneza <njenezamed@gmail.com>
2016-01-18 20:09:37 +0100

    remove unused django application

--- www/www/settings.py
+++ www/www/settings.py
@@ -32,12 +32,12 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'honeypot_web',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
+    #'django.contrib.admin',
+    #'django.contrib.auth',
+    #'django.contrib.contenttypes',
     #'django.contrib.sessions',
     #'django.contrib.messages',
-    'django.contrib.staticfiles',
+    #'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE_CLASSES = [
